68232c36559c7d4f2b63ac439d1ef73425b7924e
黄艺华 <lernanto@foxmail.com>
2021-07-01 11:46:38 +0800

    add time statistics

--- train.cc
+++ train.cc
@@ -7,6 +7,7 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include <chrono>
 
 #include "ime.h"
 
@@ -24,7 +25,13 @@ int main(int argc, char **argv)
     std::string eval_file = argv[3];
     std::string model_file = argv[4];
 
+    auto start = std::chrono::high_resolution_clock::now();
     ime::Dictionary dict(dict_file);
+    auto stop = std::chrono::high_resolution_clock::now();
+    INFO << "load dictionary "
+        << std::chrono::duration_cast<std::chrono::duration<float>>(stop - start).count()
+        << "s" << std::endl;
+
     ime::Decoder decoder(dict);
 
     size_t epochs = 2;
@@ -32,22 +39,41 @@ int main(int argc, char **argv)
     for (size_t epoch = 0; epoch < epochs; ++epoch)
     {
         std::map<std::string, double> metrics;
+
+        start = stop;
         decoder.train(train_file, metrics, batch_size);
-        INFO << "epoch " << epoch + 1 << " train" << std::endl;
+        stop = std::chrono::high_resolution_clock::now();
+
+        INFO << "epoch " << epoch + 1 << " train "
+            << std::chrono::duration_cast<std::chrono::duration<float>>(stop - start).count()
+            << "s" << std::endl;
+
         for (auto & i : metrics)
         {
             INFO << i.first << " = " << i.second << std::endl;
         }
 
         metrics.clear();
+        start = stop;
         decoder.evaluate(eval_file, metrics);
-        INFO << "evaluate" << std::endl;
+        stop = std::chrono::high_resolution_clock::now();
+
+        INFO << "evaluate "
+            << std::chrono::duration_cast<std::chrono::duration<float>>(stop - start).count()
+            << "s" << std::endl;
+
         for (auto & i : metrics)
         {
             INFO << i.first << " = " << i.second << std::endl;
         }
     }
 
+    start = stop;
     decoder.save(model_file);
+    stop = std::chrono::high_resolution_clock::now();
+    INFO << "save model "
+        << std::chrono::duration_cast<std::chrono::duration<float>>(stop - start).count()
+        << "s" << std::endl;
+
     return 0;
 }
