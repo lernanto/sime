a90b217697f17ff8e538c73df661bec0d1eea881
黄艺华 <lernanto.wong@gmail.com>
2021-07-09 20:40:51 +0800

    modify scripts to generate pinyin without tone marks

--- script/parse_pypinyin_pinyin.py
+++ script/parse_pypinyin_pinyin.py
@@ -2,17 +2,48 @@
 
 import sys
 import logging
+import re
 import unidecode
 
 
+tone_map = {
+    'ā': 'a',
+    'á': 'a',
+    'ǎ': 'a',
+    'à': 'a',
+    'ō': 'o',
+    'ó': 'o',
+    'ǒ': 'o',
+    'ò': 'o',
+    'ē': 'e',
+    'é': 'e',
+    'ě': 'e',
+    'è': 'e',
+    'ī': 'i',
+    'í': 'i',
+    'ǐ': 'i',
+    'ì': 'i',
+    'ū': 'u',
+    'ú': 'u',
+    'ǔ': 'u',
+    'ù': 'u',
+    'ǖ': 'v',
+    'ǘ': 'v',
+    'ǚ': 'v',
+    'ǜ': 'v'
+}
+
 for line in sys.stdin:
-    if not line.startswith('#'):
-        line = line.strip()
-        if line:
-            try:
-                char, _, pinyin = line.partition(':')
-                char = chr(int(char[2:], 16))
-                pinyin = unidecode.unidecode(pinyin.partition('#')[0]).replace(' ', '')
-                print('{}\t{}'.format(pinyin, char))
-            except Exception as e:
-                logging.error('{}:{}'.format(e, line))
+    line = line.partition('#')[0].strip()
+    if line:
+        try:
+            code, _, pinyins = line.partition(':')
+            char = chr(int(code[2:], 16))
+            for pinyin in pinyins.split(','):
+                pinyin = pinyin.strip()
+                if pinyin:
+                    pinyin = unidecode.unidecode(re.sub('[ǖǘǚǜ]', 'v', pinyin))
+                    print('{}\t{}'.format(pinyin, char))
+
+        except Exception as e:
+            logging.error('{}:{}'.format(e, line))
